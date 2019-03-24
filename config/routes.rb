1b2e771fee62750ffdeb12e49fdc1859f6aa192d
snsn1024 <47966782+snsn1024@users.noreply.github.com>
2019-03-24 19:10:00 +0900

    add users_show_index

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,6 @@ Rails.application.routes.draw do
 	root 'user_images#index'
 	resources :books
 	delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-
-	resources :user_images,only:[:new,:create,:index,:show,:destroy] do
-		resource :favorites, only: [:create,:destroy]
-	end
-
-	resources :user,only: [:show]
-
+	resources :user,only: [:show, :index]
 end
+
